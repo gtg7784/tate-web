769ee7687e8d772dc0bc05a79317cf1eaace584a
gtg7784 <tae.gun7784@gmail.com>
2021-03-05 00:52:54 +0900

    feat: index

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'next/link';
+import style from '../styles/header.scss'
+
+const Header = () => (
+  <header className={style.header}>
+    <img src="" alt=""/>
+    <ul>
+      <li>
+        <Link href="">HOME</Link>
+      </li>
+      <li>
+        <Link href="">ABOUT TATE</Link>
+      </li>
+      <li>
+        <Link href="">MEMBERS</Link>
+      </li>
+      <li>
+        <Link href="">ARTWORKS</Link>
+      </li>
+    </ul>
+  </header>
+);
+
+export default Header;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,35 +1,23 @@
 import React, { ReactNode } from 'react'
-import Link from 'next/link'
 import Head from 'next/head'
+import Header from './Header';
+import style from '../styles/layout.scss';
+import '../styles/main.global.scss';
 
 type Props = {
   children?: ReactNode
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
+const Layout = ({ children, title = 'Tate' }: Props) => (
+  <div className={style.body}>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="shortcut icon" href="/static/favicon.ico" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
+    <Header/>
     {children}
     <footer>
       <hr />

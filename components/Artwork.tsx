eb3588532645471a753c9602054d1dc9f2e9646a
gtg7784 <tae.gun7784@gmail.com>
2021-03-05 13:59:34 +0900

    feat: artwork component

--- components/Artwork.tsx
+++ components/Artwork.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import style from 'styles/artwork.scss'
+import { ArtworkType } from 'interfaces';
 
-type Props = {
-  img: string,
-  title: string,
-  description: string
-}
-
-const Artwork = ({ img, title, description }: Props) => (
+const Artwork = ({ img, name, description }: ArtworkType) => (
   <div className={style.artwork}>
-    <img src={img} alt=""/>
-    <h4>{title}</h4>
+    <img src={img} alt={name}/>
+    <h4>{name}</h4>
     <p>{description}</p>
   </div>
 )
